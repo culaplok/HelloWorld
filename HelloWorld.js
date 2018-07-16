e2370989c4b4ba5e28b8e82696cc90a4cdacda35
Nick Chan 詹記翔 #8072 <nick.chan@mpi.com.tw>
2018-07-16 14:33:25 +0800

    tree

--- HelloWorld.js
+++ HelloWorld.js
@@ -2,6 +2,6 @@ var http = require('http');
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.end('Hello World!');
-}).listen(8080);
+}).listen(80);
 
 console.log('Server is Run');
